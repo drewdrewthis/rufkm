d338d4b651d8488570ad222865f4184a7bb45532
drew <drewdrewthis@gmail.com>
2019-09-08 17:23:58 -0400

    Update test

--- utils/__tests__/index.spec.js
+++ utils/__tests__/index.spec.js
@@ -1,5 +1,13 @@
-import { parseText } from '..'
+import { parseText, sampleMessageGenerator } from '..'
+import omit from 'lodash/omit';
 import sampleConversationText from '../../sampleData/sampleConversationText';
+
+const DATE_TO_USE = new Date('2020');
+const _Date = Date;
+global.Date = jest.fn(() => DATE_TO_USE);
+global.Date.UTC = _Date.UTC;
+global.Date.parse = _Date.parse;
+global.Date.now = _Date.now;
 
 describe('parseText', () => {
   it('parses the text', () => {
@@ -49,3 +57,55 @@ describe('parseText', () => {
       ])
   })
 })
+
+describe('sampleMessageGenerator', () => {
+  it('can create one message', () => {
+
+    const parsedText = { user: 'A', text: 'Hi Jack. What are you doing?' };
+    const result = sampleMessageGenerator(parsedText);
+
+    expect(result).toEqual({
+      _id: 1,
+      text: 'Hi Jack. What are you doing?',
+      createdAt: new Date(),
+      user: {
+        _id: 1,
+        name: 'A',
+        avatar: "https://placeimg.com/140/140/any"
+      }
+    })
+
+  })
+
+  it('can create many', () => {
+    const textArray = parseText(sampleConversationText);
+    const messageArray = textArray.map(sampleMessageGenerator);
+    messageArray.length = 4;
+
+    expect(messageArray).toEqual([
+      {
+        "_id": 2,
+        "createdAt": new Date(),
+        "text": "Hi Jack. What are you doing?",
+        "user": {
+          "_id": 1, "avatar": "https://placeimg.com/140/140/any",
+          "name": "A"
+        }
+      },
+      {
+        "_id": 3,
+        "createdAt": new Date(),
+        "text": "Hi Mary. I'm filling out a job application.",
+        "user": { "_id": 2, "avatar": "https://placeimg.com/140/140/any", "name": "B" }
+      }, {
+        "_id": 4, "createdAt": new Date(), "text": "Are you finished with school already?",
+        "user": { "_id": 1, "avatar": "https://placeimg.com/140/140/any", "name": "A" }
+      },
+      {
+        "_id": 5, "createdAt": new Date(),
+        "text": "No. I have one more semester, but it would be great to have a job lined up.",
+        "user": { "_id": 2, "avatar": "https://placeimg.com/140/140/any", "name": "B" }
+      },])
+
+  })
+});
--- utils/index.ts
+++ utils/index.ts
@@ -1,4 +1,8 @@
 import compact from 'lodash/compact';
+import { IMessage } from 'react-native-gifted-chat';
+
+let id = 1;
+const userIds = {}
 
 export const parseText = (text: string) => {
   const lines = text.split('\n');
@@ -15,19 +19,21 @@ export const parseText = (text: string) => {
       text
     }
   }));
-
 }
 
-export const sampleMessageGenerator = (parsedText) => {
-  const text =
-  {
-    "_id": 1,
+export const sampleMessageGenerator = (parsedText): IMessage => {
+  const name = parsedText.user;
+  userIds[name] = userIds[name] || Object.keys(userIds).length + 1;
+
+  return {
+    "_id": id++,
     text: parsedText.text,
-    "createdAt": "2019-09-06T11:51:01.990Z",
+    // "createdAt": "2019-09-06T11:51:01.990Z",
+    "createdAt": new Date(),
     "user": {
-      "_id": 2,
-      "name": "React Native",
+      "_id": userIds[name],
+      name,
       "avatar": "https://placeimg.com/140/140/any"
     }
-  },
+  }
 };
